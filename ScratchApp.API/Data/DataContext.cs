0b7f8c42a9f4beb813d3ca9f8b48192b874a1a05
shamim08sust <shamim08sust@gmail.com>
2021-05-31 13:21:14 +0300

    initial commit

---
+++ ScratchApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ScratchApp.API.Models;
+
+namespace ScratchApp.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        {           
+        }
+        public DbSet<Values> Values { get; set; }
+    }
+}
---
+++ ScratchApp.API/Models/values.cs
@@ -0,0 +1,8 @@
+namespace ScratchApp.API.Models
+{
+   public class Values
+    {
+        public int Id { get; set; }       
+        public string Name { get; set; }  
+    }
+}
